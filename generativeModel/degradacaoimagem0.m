cd8e46fba78b800f06fce84030b34743f390136c
David Anchieta <davidca102@gmail.com>
2017-03-09 15:26:12 -0300

    feita função que gera matriz de covariancia da FDP a priori

--- generativeModel/degradacaoimagem0.m
+++ generativeModel/degradacaoimagem0.m
@@ -4,6 +4,11 @@ clc
 
 load dadosimagem
 
-dkpb = imageDegrade(kpb, .5, 2, 0, [35 ; 50], 0);
+dkpb = imageDegrade(kpb,...
+    1,... %fator de degradacao
+    2,... %gamma da PSF
+    0,... %angulo de rotacao
+    [0 ; 0],... %deslocamento em x e y
+    0); %desvio padrão do ruído
 
 imshow(dkpb);
